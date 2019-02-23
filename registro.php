ed35c833c60e1d9e65e95f96bfb48f1ac50a9bfe
Jhonatan Freitas <itjhonatan01@gmail.com>
2019-02-22 21:32:55 -0300

    criando buscaCep

---
+++ registro.php
@@ -0,0 +1,24 @@
+<?php
+  require('./conexao.php');
+
+  if($_SERVER['REQUEST_METHOD'] === 'POST') {
+      $cep          =     $_POST['txtcep'];
+      $logradouro   =     $_POST['txtlogradouro'];
+      $bairro       =     $_POST['txtbairro'];
+      $cidade       =     $_POST['txtcidade'];
+      $estado       =     $_POST['txtestado'];
+      $complemento  =     $_POST['txtcomplemento'];
+
+      $link = getConnection();
+   
+    $query = "insert into tb_enderecos values('{$cep}', '{$logradouro}', '{$bairro}', '{$cidade}', '{$estado}', '{$complemento}')";
+    echo $query;
+    exit;
+    try {
+        mysqli_query($link, $query);
+        return true;
+    } catch (\Throwable $th) {
+        throw new \Exception("Error ao gravar", 1);
+        return false;
+    }
+  }
